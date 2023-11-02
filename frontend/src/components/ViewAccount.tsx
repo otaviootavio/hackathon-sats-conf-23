0bab7d3d4c60947832e434908816d90f9e7f98f8
otaviootavio <otaviovacari@usp.br>
2023-11-02 17:24:31 -0300

    add components

---
+++ frontend/src/components/ViewAccount.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const ViewAccount = (props: Props) => {
+  //todo
+  // mock the account data
+    return (
+    <div>ViewAccount</div>
+  )
+}
+
+export default ViewAccount
