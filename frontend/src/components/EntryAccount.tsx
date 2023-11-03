2ee69dfd99d9430dfee5f2d1f255c1fb56c97fc4
otaviootavio <otaviovacari@usp.br>
2023-11-03 11:55:42 -0300

    calculate account balance

--- frontend/src/components/EntryAccount.tsx
+++ frontend/src/components/EntryAccount.tsx
@@ -4,6 +4,23 @@ import AccountsView from "./AccountsView";
 import AddAccountForms from "./AddAccountForms";
 import Account from "./types/accountModel";
 
+async function calculateDebtAndCredit(transList: Account[]): Promise<Record<string, number>> {
+  const balances: Record<string, number> = {};
+
+  transList.forEach((transaction: Account) => {
+    const { from, to, amount } = transaction;
+
+    // Initialize or update the balance for the 'from' account
+    balances[from] = (balances[from] || 0) - parseFloat(amount);
+
+    // Initialize or update the balance for the 'to' account
+    balances[to] = (balances[to] || 0) + parseFloat(amount);
+  });
+
+  return balances;
+}
+
+
 const myAccounts: Accounts = {
   id: [
     {
@@ -36,6 +53,7 @@ const EntryAccount = () => {
   const [accounts, setAccounts] = useState<Account[]>(myArrayAccounts);
   const [currentAccountGroup, setCurretnAccountGroup] = useState<string>("");
   const [newAccountGroupId, setNewAccountGroupId] = useState<string>("");
+  const [balances, setBalances] = useState<Record<string, number>>({});
 
   const onSave = (newAccount: Account) => {
     setAccounts([...accounts, newAccount]);
@@ -47,6 +65,12 @@ const EntryAccount = () => {
     setAccounts(myArrayAccounts.filter((e) => {return e.groupId === newAccountGroupId}))
     console.log(newAccountGroupId);
   };
+
+  const handleEndTheSplit = async () => {
+    const result: Record<string, number> = await calculateDebtAndCredit(accounts);
+    setBalances(result)
+    console.log(result)
+  }
 
   return (
     <>
@@ -76,12 +100,29 @@ const EntryAccount = () => {
         <section>
           <aside>
             <AccountsView accountArray={accounts} />
+            <input type="button" onClick={handleEndTheSplit} value={"End the split!"}/>
           </aside>
           <>
             <AddAccountForms onSave={onSave} />
           </>
         </section>
       )}
+      <section>
+        <aside>
+        <div>
+      <h2>Account Balances</h2>
+      <button onClick={handleEndTheSplit}>Calculate Debts and Credits</button>
+      <div>
+        <h3>Results:</h3>
+        {Object.entries(balances).map(([account, balance]) => (
+          <div key={account}>
+            {account}: {balance < 0 ? `Debt: ${Math.abs(balance)}` : `Credit: ${balance}`}
+          </div>
+        ))}
+      </div>
+    </div>
+        </aside>
+      </section>
     </>
   );
 };
